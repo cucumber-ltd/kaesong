9ef6a2cd07f9ce0d9a56ead267a94de4b6c93b46
Steve Tooke <tooky@cucumber.io>
2017-11-23 16:13:53 +0000

    Allow configuration of the log tag

--- test/log_test.js
+++ test/log_test.js
@@ -4,9 +4,18 @@ const assert = require('assert')
 const Log = require('../lib/log')
 
 describe('log', () => {
-  it('generates a log name that includes file name', () => {
-    const [logTag, logPath] = Log.name(__filename).split(':')
-    assert.equal(logTag, 'kaesong')
+  it('generates a log label that includes file name', () => {
+    const [logTag, logPath] = Log.label(__filename).split(':')
     assert(__filename.endsWith(logPath))
+  })
+
+  it('allows you to set the log tag', () => {
+    const [logTag] = Log('cpro').label(__filename).split(':')
+    assert.equal(logTag, 'cpro')
+  })
+
+  it('defaults to tagging the log name with kaesong', () => {
+    const [logTag] = Log.label(__filename).split(':')
+    assert.equal(logTag, 'kaesong')
   })
 })
